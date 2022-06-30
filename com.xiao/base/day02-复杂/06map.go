b7ebaba1b5619e0c495ef4d214bc776111f3ec64
lktbz <617387614@qq.com>
2022-06-30 22:31:38 +0800

    map 类型学完

--- com.xiao/base/day02-复杂/06map.go
+++ com.xiao/base/day02-复杂/06map.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	m:=make(map[string]int)
+	var mapLit map[string]int  //创建方式一
+	mapLit=map[string]int{"one":1,"two":2} //创建方式二
+  	fmt.Println(mapLit)
+	m:=make(map[string]int)//创建方式三
 	m["k1"]=7
 	m["k2"]=13
 	fmt.Println("map:",m)
@@ -13,6 +16,41 @@ func main() {
 
 	fmt.Println("len():",len(m))
 
+	delete(m,"k2")
+	fmt.Println("map",m)
 
+    _, prs:=m["k2"]
+	fmt.Println("prs:", prs)
 
+	var value int
+	var isPresent bool
+    map1:=make(map[string]int)
+	map1["New Delhi"] = 55
+	map1["Beijing"] = 20
+	map1["Washington"] = 25
+	value, isPresent =map1["Beijing"]
+	if isPresent {
+		fmt.Printf("The value of \"Beijing\" in map1 is: %d\n", value)
+	} else {
+		fmt.Printf("map1 does not contain Beijing")
+	}
+	value, isPresent = map1["Paris"]
+	fmt.Printf("Is \"Paris\" in map1 ?: %t\n", isPresent)
+	fmt.Printf("Value is: %d\n", value)
+	map12 := make(map[int]float32)
+	map12[1] = 1.0
+	map12[2] = 2.0
+	map12[3] = 3.0
+	map12[4] = 4.0
+	for i, v := range map12 {
+		fmt.Printf("key is: %d - value is: %f\n", i, v)
+	}
+	capitals := map[string] string {"France":"Paris", "Italy":"Rome", "Japan":"Tokyo" }
+	for skey  := range capitals {
+		fmt.Println("Map item: Capital of", skey, "is", capitals[skey])
+	}
+	for _ ,value := range capitals {
+		fmt.Println("Map item: Capital value", value)
+	}
+	
 }
